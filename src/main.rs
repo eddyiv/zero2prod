26728be391e9ac5cda81a5f5ffa1566cfcd77b49
Eddy Rogers <eddyrogersiv@gmail.com>
2026-08-27 08:52:54 -0500

    With Added Files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+use std::net::TcpListener;
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    run(TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000"))?.await
 }
